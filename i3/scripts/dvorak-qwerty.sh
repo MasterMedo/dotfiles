9df14aad7f8b2f3d9fc6833d4ac080e06a11ac81
MasterMedo <mislav.vuletic@gmail.com>
2020-01-26 20:28:18 +0100

    update: dvorak-qwerty.sh

--- i3/scripts/dvorak-qwerty.sh
+++ i3/scripts/dvorak-qwerty.sh
@@ -3,6 +3,9 @@ if [ $(setxkbmap -query | grep variant | tail -c -7) = "dvorak" ]
 then
 	setxkbmap us
 else
-	setxkbmap -variant dvorak -layout us
+	#setxkbmap -variant dvorak -layout us
+	#setxkbmap -variant colemak -layout us
+	setxkbmap gr
+	#setxkbmap ru
 fi
 
