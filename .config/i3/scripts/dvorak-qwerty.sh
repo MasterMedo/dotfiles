401198ea73baad5952dd100fdab5b65aa3758d98
MasterMedo <mislav.vuletic@gmail.com>
2020-03-15 19:41:25 +0100

    update: tabs -> spaces

--- .config/i3/scripts/dvorak-qwerty.sh
+++ .config/i3/scripts/dvorak-qwerty.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 if [ $(setxkbmap -query | grep variant | tail -c -7) = "dvorak" ]
 then
-	setxkbmap us
+  setxkbmap us
 else
-	#setxkbmap -variant dvorak -layout us
-	#setxkbmap -variant colemak -layout us
-	setxkbmap gr
-	#setxkbmap ru
+  #setxkbmap -variant dvorak -layout us
+  #setxkbmap -variant colemak -layout us
+  setxkbmap gr
+  #setxkbmap ru
 fi
-
--- .config/i3/scripts/toggleKBlayout.sh
+++ .config/i3/scripts/toggleKBlayout.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [ $(setxkbmap -query | grep layout | tail -c -3) = "us" ]
 then
-	setxkbmap hr
+  setxkbmap hr
 else
-	setxkbmap us
+  setxkbmap us
 fi
-
