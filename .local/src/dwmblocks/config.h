d93299114fd7c1ca25d065ae989a36ebff3237a4
MasterMedo <mislav.vuletic@gmail.com>
2020-12-03 16:05:38 +0100

    dwm status bar update

--- .local/src/dwmblocks/config.h
+++ .local/src/dwmblocks/config.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
   {"", "cat /tmp/recordingicon 2>/dev/null",  0,  9},
   {"🔋", "cat /sys/class/power_supply/BAT0/capacity",  5,  3},
   {"% ", "setxkbmap -query | grep layout | tail -c -3", 0, 30}, //⌨
-  {" ☔",  "weather", 18000,  5},
+  {" ",  "weather", 18000,  5},
   {" 📆",  "date '+%d.%m.%Y. (%a)'",  60, 1},
   {" 🕗",  "date '+%H:%M '",  60, 1},
 };
