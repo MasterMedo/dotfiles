bac3cc09924a31e31eedb35ecf3b57e2c1ac17ef
MasterMedo <mislav.vuletic@gmail.com>
2021-05-09 21:40:32 +0200

    fish: remove wal, change stock colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,26 +7,27 @@ if status is-login
 end
 
 # cat ~/.cache/wal/sequences
-if status is-interactive
-  cat ~/.cache/wal/sequences
-end
+# if status is-interactive
+#   cat ~/.cache/wal/sequences
+# end
+tput smkx
 
 set fish_color_normal 'normal'
 set fish_color_command 'brwhite' --bold
-set fish_color_quote 'magenta'
+set fish_color_quote 'red'
 set fish_color_redirection 'brred'
 set fish_color_end '00d700'
 set fish_color_error 'ff0000'
 set fish_color_param 'cyan'
 set fish_color_comment 'yellow'
-set fish_color_match --background='white'
+set fish_color_match --background='black'
 set fish_color_selection --background='brblack'
 set fish_color_search_match --background='brblack'
-set fish_color_operator 'green' --bold
+set fish_color_operator 'yellow' --bold
 set fish_color_escape 'brred' --bold
-set fish_color_cwd 'green' --bold
+set fish_color_cwd 'yellow' --bold
 set fish_color_autosuggestion 'brblack'
 set fish_color_user 'magenta' --bold
-set fish_color_host 'green' --bold
+set fish_color_host 'yellow' --bold
 set fish_color_host_remote 'red' --bold
 set fish_color_cancel 'ff0000' --bold
